e864793ebea58eabaf7a1f7e1666e7f2a498c474
David Svensson <davidsvson@gmail.com>
2020-04-03 12:01:52 +0200

    add new activity

--- app/src/main/java/com/example/lektion2android/MainActivity.kt
+++ app/src/main/java/com/example/lektion2android/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+
+const val EXTRA_MESSAGE = "com.example.lektion2android.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
     //lateinit för vi inte vill att den ska kunna inehålla null
     // men vi kan inte redan nu ge den ett värde
     lateinit var questionView : TextView
+    var firstNumber = 0
+    var secondNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,26 +25,49 @@ class MainActivity : AppCompatActivity() {
         questionView = findViewById(R.id.questionText)
         randomizeQuestion()
 
-
         val button = findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
             startAnswerActivity()
         }
+    }
 
+    override fun onRestart() {
+        super.onRestart()
+
+        randomizeQuestion()
+    }
+
+
+    fun checkAnswer() : Boolean {
+        val answerText = findViewById<EditText>(R.id.answerText)
+        val answer = answerText.text.toString()
+        val answerInt = answer.toIntOrNull()
+
+        return firstNumber + secondNumber == answerInt
     }
 
     fun startAnswerActivity() {
+        val rightAnswer = checkAnswer()
+
         //intent:et innehåller vilken activity vi är i och vilken activity vi ska till
-        val intent = Intent(this, AnswerActivity::class.java)
+
+        val intent = Intent(this, AnswerActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, rightAnswer)
+        }
+
+        // i stället för att at det indent vi just skapa och göra .putextra
+        // så gör vi som ovanstående kör .apply och läg där inne
+        // det som vi vill ska aplliceras på det just skapade objektet
+        // intent.putExtra(EXTRA_MESSAGE, rightAnswer )
 
         //skicka med vårt intent till startactivity metoden
         startActivity(intent)
     }
 
     fun randomizeQuestion() {
-        val firstNumber = (1..10).random()
-        val secondNumber = (1..10).random()
+        firstNumber = (1..10).random()
+        secondNumber = (1..10).random()
 
         questionView.text = "$firstNumber + $secondNumber ="
     }
